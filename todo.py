1521f97984cb6466f8e07db0d1478f54719773e3
Siddique <farhansiddique9772@gmail.com>
2026-05-18 01:37:13 +0530

    added file based savings to store the data even after closing the file

--- todo.py
+++ todo.py
@@ -17,6 +17,44 @@ delete notes
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+try:
+    with open("data.txt", "x") as file:
+        file.read()
+except FileExistsError:
+    pass
+
+def load(li):
+    with open("data.txt", "a") as f:
+            f.write(li + "\n")
+
+def delt():
+    with open("data.txt", "r",encoding="utf-8") as f:
+        lines = f.read().splitlines()
+        dele = questionary.select("select what you want to delete:", choices=lines).ask()
+    
+    lines.remove(dele)
+
+    with open("data.txt", "w",encoding="utf-8") as f:
+        for line in lines:
+            f.write(line + "\n")
+
+def upd():
+    with open("data.txt", "r",encoding="utf-8") as f:
+        lines = f.read().splitlines()
+        upd = questionary.select("select what you want to update:", choices=lines).ask()
+    
+        text = input("enter the updated task:")
+        print(lines)
+                
+        for i,item in enumerate(lines):
+            if item == upd:
+                lines[i] = text
+    
+
+    with open("data.txt", "w",encoding="utf-8") as f:
+        for line in lines:
+            f.write(line + "\n")
+
 
 
 
@@ -38,55 +76,56 @@ while True:
 
         case "add task":
            task =  input("enter your task:")
-           l.append(task)
+           load(task)
            print("TASK ADDED SUCCESFULLY")
            time.sleep(0.5)
            clear_screen()
            continue
         
         case "view task":
-            if not l:
-                print("there are no tasks, add a task to view it!!")
-                print("\n")
-
-            else :
-                for row in l:
-                    print(str(l.index(row)+1)+"." + row)
-                
-                print("\n")
+            clear_screen()
+            
+            with open("data.txt", "r",encoding="utf-8") as f:
+                for i,line in enumerate(f):
+                    print(i+1,".",line.strip())
 
             continue
 
         
 
         case "delete task":
-            if not l:
-                print("there are no tasks, add a task to delete it!!")
-                continue
+            delt()
+            # if not l:
+            #     print("there are no tasks, add a task to delete it!!")
+            #     continue
 
-            delt = questionary.select("select the task you want to delete:",choices = l).ask()
-            # delt =  int(input("enter the task number you want to delete: "))
-            l.remove(delt)
-            print(f"deleted the task, the remaining tasks are: \n")
-            for row in l:
-                print(str(l.index(row)+1)+"." + row + "\n")
+            # delt = questionary.select("select the task you want to delete:",choices = l).ask()
+            # # delt =  int(input("enter the task number you want to delete: "))
+            # l.remove(delt)
+            # print(f"deleted the task, the remaining tasks are: \n")
+            # for row in l:
+            #     print(str(l.index(row)+1)+"." + row + "\n")
 
             clear_screen()
+            print("XXXXXXXXX-----TASK DELETED-----XXXXXXXXX")
             
 
         case "update task":
+            
 
-            if not l:
-                print("there are no tasks, add a task to update it!!")
-                continue
+            upd()
 
-            upd = questionary.select("select the tasks from below to update: ",choices =l).ask()
+            # if not l:
+            #     print("there are no tasks, add a task to update it!!")
+            #     continue
 
-            text = input("enter the updated task:")
+            # upd = questionary.select("select the tasks from below to update: ",choices =l).ask()
+
+            # text = input("enter the updated task:")
              
-            for i,item in enumerate(l):
-                if item == upd:
-                    l[i] = text
+            # for i,item in enumerate(l):
+            #     if item == upd:
+            #         l[i] = text
                 
 
 
@@ -96,20 +135,23 @@ while True:
 
         case "mark done":
 
-            if not l:
-                print("there are no tasks, add a task to mark it!!")
-                continue
+            # if not l:
+            #     print("there are no tasks, add a task to mark it!!")
+            #     continue
 
-            comp = questionary.select("select the tasks from below to mark as completed: ",choices =l).ask()
+            with open("data.txt", "r",encoding="utf-8") as f:
+                
+                lines = f.read().splitlines()
+                comp = questionary.select("select the tasks from below to mark as completed: ",choices = lines).ask()
 
-            for i,item in enumerate(l):
+            for i,item in enumerate(lines):
                 if item == comp:
-                    l[i] = l[i] + "✓"
+                    lines[i] = lines[i] + "✓"
 
-                    for row in l:
-                        print(row)
-
-                    del l[i]           
+            with open("data.txt","w",encoding="utf-8") as f:
+                for line in lines:
+                    f.write(line + "\n")
+                         
             
         case "exit":
             break
